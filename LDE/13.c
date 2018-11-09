d62329c30f6f08af7449b2b34d2963a9fde44235
Rayner <raynersulyak@gmail.com>
2018-11-09 21:36:03 -0200

    fixed c++ style function

--- LDE/13.c
+++ LDE/13.c
@@ -3,7 +3,7 @@
 #include "node.h"
 
 void cria_lista_ord();
-
+void inserir_ordenado(int data);
 struct node *inicio;
 
 int main(){
@@ -12,44 +12,6 @@ int main(){
 }
 
 void cria_lista_ord(){
-    void inserir_ordenado(int data){   
-        struct node *aux;
-        //lista vazia
-        if(inicio == (struct node *)NULL){
-            inicio = (struct node *)malloc(sizeof(struct node));
-            inicio->data = data;
-            inicio->next = (struct node *)NULL;
-            inicio->prev = (struct node *)NULL; 
-        }
-        // inserir antes do inicio
-        else if(inicio->data > data){
-            inicio->prev = (struct node *)malloc(sizeof(struct node));
-            inicio->prev->next = inicio;
-            inicio = inicio->prev;
-            inicio->data = data;
-        }
-        else{
-            aux = inicio;
-            while(aux->next != (struct node *)NULL && aux->data <= data)
-                aux = aux->next;
-            //inserir antes de aux
-            if(aux->data > data){
-                aux->prev->next = (struct node *)malloc(sizeof(struct node));
-                aux->prev->next->next = aux;
-                aux->prev->next->data = data;
-                aux->prev->next->prev = aux->prev;
-                aux->prev = aux->prev->next;
-            }
-            //inserir dps de aux 
-            else{ // (aux->data <= data)
-                aux->next = (struct node *)malloc(sizeof(struct node));
-                aux->next->prev = aux;
-                aux->next->next = (struct node *)NULL;
-                aux->next->data = data;
-            }
-        }
-        aux = (struct node *)NULL;
-    }
     int data;
     inicio = (struct node *)NULL;
     scanf("%d", &data);
@@ -58,4 +20,43 @@ void cria_lista_ord(){
         scanf("%d", &data);
     }
 
+}
+
+void inserir_ordenado(int data){   
+    struct node *aux;
+    //lista vazia
+    if(inicio == (struct node *)NULL){
+        inicio = (struct node *)malloc(sizeof(struct node));
+        inicio->data = data;
+        inicio->next = (struct node *)NULL;
+        inicio->prev = (struct node *)NULL; 
+    }
+    // inserir antes do inicio
+    else if(inicio->data > data){
+        inicio->prev = (struct node *)malloc(sizeof(struct node));
+        inicio->prev->next = inicio;
+        inicio = inicio->prev;
+        inicio->data = data;
+    }
+    else{
+        aux = inicio;
+        while(aux->next != (struct node *)NULL && aux->data <= data)
+            aux = aux->next;
+        //inserir antes de aux
+        if(aux->data > data){
+            aux->prev->next = (struct node *)malloc(sizeof(struct node));
+            aux->prev->next->next = aux;
+            aux->prev->next->data = data;
+            aux->prev->next->prev = aux->prev;
+            aux->prev = aux->prev->next;
+        }
+        //inserir dps de aux 
+        else{ // (aux->data <= data)
+            aux->next = (struct node *)malloc(sizeof(struct node));
+            aux->next->prev = aux;
+            aux->next->next = (struct node *)NULL;
+            aux->next->data = data;
+        }
+    }
+    aux = (struct node *)NULL;
 }
