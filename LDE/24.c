6aada406c46306167192eac52aa4f44242b3c412
Rayner <raynersulyak@gmail.com>
2018-11-15 16:34:56 -0200

    adjusted for c++ style functions

--- LDE/24.c
+++ LDE/24.c
@@ -8,56 +8,17 @@ void unir_rec(struct node *aux, struct node *first, struct node *second);
 
 int main(){
     struct node *first, *second;
-    cria_lista_2(&first);
-    cria_lista_2(&second);
+    cria_lista(&first);
+    cria_lista(&second);
     printf("Primeira:\n");
-    show_content_debug_2(first);
+    debug(first);
     printf("Segunda:\n");
-    show_content_debug_2(second);
+    debug(second);
     unir_rec((struct node *)NULL, first, second);
-    show_content_debug();
+    debug();
 }
 
 void unir_rec(struct node *aux, struct node *first, struct node *second){
-    void inserir_ordenado_hard(int data){
-        struct node *aux;
-        //lista vazia
-        if(inicio == (struct node *)NULL){
-            inicio = (struct node *)malloc(sizeof(struct node));
-            inicio->data = data;
-            inicio->next = (struct node *)NULL;
-            inicio->prev = (struct node *)NULL; 
-        }
-        // inserir antes do inicio
-        else if(inicio->data > data){
-            inicio->prev = (struct node *)malloc(sizeof(struct node));
-            inicio->prev->next = inicio;
-            inicio->prev->data = data;
-            inicio->prev->prev = (struct node *)NULL;
-            inicio = inicio->prev;
-        }
-        else{
-            aux = inicio;
-            while(aux->next != (struct node *)NULL && aux->data <= data)
-                aux = aux->next;
-            //inserir antes de aux
-            if(aux->data > data){
-                aux->prev->next = (struct node *)malloc(sizeof(struct node));
-                aux->prev->next->prev = aux->prev;
-                aux->prev->next->next = aux;
-                aux->prev->next->data = data;
-                aux->prev = aux->prev->next;
-            }
-            //inserir dps de aux 
-            else{ // (aux->data <= data)
-                aux->next = (struct node *)malloc(sizeof(struct node));
-                aux->next->prev = aux;
-                aux->next->next = (struct node *)NULL;
-                aux->next->data = data;
-            }
-        }
-        aux = (struct node *)NULL;
-    }
     if(aux = (struct node *)NULL)
         inicio = (struct node *)NULL;
     if(first != (struct node *)NULL || second != (struct node *)NULL){
@@ -76,57 +37,42 @@ void unir_rec(struct node *aux, struct node *first, struct node *second){
     aux = (struct node *)NULL;
 }
 
-void unir(struct node *first, struct node *second){
-    void inserir_ordenado_hard(int data){
-        struct node *aux;
-        //lista vazia
-        if(inicio == (struct node *)NULL){
-            inicio = (struct node *)malloc(sizeof(struct node));
-            inicio->data = data;
-            inicio->next = (struct node *)NULL;
-            inicio->prev = (struct node *)NULL; 
-        }
-        // inserir antes do inicio
-        else if(inicio->data > data){
-            inicio->prev = (struct node *)malloc(sizeof(struct node));
-            inicio->prev->next = inicio;
-            inicio->prev->data = data;
-            inicio->prev->prev = (struct node *)NULL;
-            inicio = inicio->prev;
-        }
-        else{
-            aux = inicio;
-            while(aux->next != (struct node *)NULL && aux->data <= data)
-                aux = aux->next;
-            //inserir antes de aux
-            if(aux->data > data){
-                aux->prev->next = (struct node *)malloc(sizeof(struct node));
-                aux->prev->next->prev = aux->prev;
-                aux->prev->next->next = aux;
-                aux->prev->next->data = data;
-                aux->prev = aux->prev->next;
-            }
-            //inserir dps de aux 
-            else{ // (aux->data <= data)
-                aux->next = (struct node *)malloc(sizeof(struct node));
-                aux->next->prev = aux;
-                aux->next->next = (struct node *)NULL;
-                aux->next->data = data;
-            }
-        }
-        aux = (struct node *)NULL;
-    }
+void inserir_ordenado_hard(int data){
     struct node *aux;
-    inicio = (struct node *)NULL;
-    aux = first;
-    while(aux != (struct node *)NULL){
-        inserir_ordenado_hard(aux->data);
-        aux = aux->next;
+    //lista vazia
+    if(inicio == (struct node *)NULL){
+        inicio = (struct node *)malloc(sizeof(struct node));
+        inicio->data = data;
+        inicio->next = (struct node *)NULL;
+        inicio->prev = (struct node *)NULL; 
     }
-    aux = second;
-    while(aux != (struct node *)NULL){
-        inserir_ordenado_hard(aux->data);
-        aux = aux->next;
+    // inserir antes do inicio
+    else if(inicio->data > data){
+        inicio->prev = (struct node *)malloc(sizeof(struct node));
+        inicio->prev->next = inicio;
+        inicio->prev->data = data;
+        inicio->prev->prev = (struct node *)NULL;
+        inicio = inicio->prev;
+    }
+    else{
+        aux = inicio;
+        while(aux->next != (struct node *)NULL && aux->data <= data)
+            aux = aux->next;
+        //inserir antes de aux
+        if(aux->data > data){
+            aux->prev->next = (struct node *)malloc(sizeof(struct node));
+            aux->prev->next->prev = aux->prev;
+            aux->prev->next->next = aux;
+            aux->prev->next->data = data;
+            aux->prev = aux->prev->next;
+        }
+        //inserir dps de aux 
+        else{ // (aux->data <= data)
+            aux->next = (struct node *)malloc(sizeof(struct node));
+            aux->next->prev = aux;
+            aux->next->next = (struct node *)NULL;
+            aux->next->data = data;
+        }
     }
     aux = (struct node *)NULL;
 }
